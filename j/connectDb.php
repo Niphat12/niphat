26748ce62b830090ea470efb6f5cb8cbcfe49fbb
Niphat12 <66010914146@msu.ac.th>
2026-02-17 12:14:40 +0700

    Add files via upload

---
+++ j/connectDb.php
@@ -0,0 +1,10 @@
+<?php
+                $host ="localhost";  
+                $user = "root";
+                $pwd = "123456789";
+                $DB = "4146"; 
+                #เชื่อมต่อฐานข้อมูล
+                $conn = mysqli_connect($host,$user,$pwd,$DB) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
+                mysqli_query($conn, "SET NAMES utf8");
+
+?>
